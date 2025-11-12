460a2604b29606535a0ae1133c4527d3b22b714d
rajalishikhe <nahidujjamanraj@gmail.com>
2025-11-12 11:16:56 +0600

    first commit

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Component/Root/Root";
+
+const Router =createBrowserRouter([
+  {
+    path: "/",
+    Component:Root
+  }
+]);
+export default Router;
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
+import './index.css'
+import Router from './Router/Router.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={Router} />
+  </StrictMode>
+)
